f3c1c3b211de7e13aa4142504f95637c89c41b56
Nick Hartjes <nick@hartj.es>
2026-04-25 14:08:03 +0200

    chore(nix): add flake inputs (nixpkgs, flake-utils, crane, rust-overlay)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,15 @@
 {
   description = "Terminal UI for the just command runner";
 
-  outputs = { self }: { };
+  inputs = {
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url       = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay }: { };
 }
